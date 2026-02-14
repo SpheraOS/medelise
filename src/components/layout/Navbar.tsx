bb0118dcf3a9dbb4ed80367089e18dec152cd78e
MIRCEA POPA <mi@MacBook-Air-MIRCEA.local>
2026-02-14 19:08:30 +0200

    fix: align nav breakpoints with CSS - iPad Pro gets TabletNav
    - Desktop nav: min-[1025px] (was lg/1024px)
    - Tablet nav: min-[641px] to max-[1024px] (was md to lg)
    - Mobile nav: below 641px (was below md/768px)
    - iPad Pro 1024px now correctly shows TabletNav

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -7,18 +7,18 @@ import MobileNav from './MobileNav';
 export default function Navbar() {
     return (
         <div className="absolute top-0 left-0 w-full z-50">
-            {/* Desktop Navigation - visible on screens >= 1024px */}
-            <div className="hidden lg:block">
+            {/* Desktop Navigation - visible on screens >= 1025px */}
+            <div className="hidden min-[1025px]:block">
                 <DesktopNav />
             </div>
 
-            {/* Tablet Navigation - visible on screens 768px - 1023px */}
-            <div className="hidden md:block lg:hidden">
+            {/* Tablet Navigation - visible on screens 641px - 1024px */}
+            <div className="hidden min-[641px]:block min-[1025px]:hidden">
                 <TabletNav />
             </div>
 
-            {/* Mobile Navigation - visible on screens < 768px */}
-            <div className="block md:hidden">
+            {/* Mobile Navigation - visible on screens < 641px */}
+            <div className="block min-[641px]:hidden">
                 <MobileNav />
             </div>
         </div>
